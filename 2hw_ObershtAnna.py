6248a2bc82f56aa11f556b1c2476cde91ae084db
Anna Obersht <obershtav22@gmail.com>
2023-06-23 20:31:23 +0300

    Initial commit

---
+++ 2hw_ObershtAnna.py
@@ -0,0 +1,13 @@
+#!C:\Users\CommonHome\AppData\Local\Programs\Python\Python311\python.exe
+#if __name__== '___mine_'
+#-*- coding: utf-8 -*-
+# coding: utf-8
+
+import sys
+print (sys.version)
+
+for arg in sys.argv:
+    print(arg)
+print('Вітаю,', sys.argv[1], sys.argv[2], sys.argv[3])  
+
+#print("Привіт, Обершт Анна Володимирівна.")
